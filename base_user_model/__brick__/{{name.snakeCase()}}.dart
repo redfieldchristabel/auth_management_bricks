26eee9cacc6fbe3035c5abf2c053825da78c5962
imano00 <nurimanjamal00@gmail.com>
2023-05-03 15:34:53 +0800

    change spacing

--- base_user_model/__brick__/{{name.snakeCase()}}.dart
+++ base_user_model/__brick__/{{name.snakeCase()}}.dart
@@ -1,6 +1,6 @@
 import 'package:auth_management/auth_management.dart';
-{{#isSerializable}}
-import 'package:json_annotation/json_annotation.dart';
+{{#isSerializable}}import 'package:json_annotation/json_annotation.dart';
+
 
 part '{{name.snakeCase()}}.g.dart';
 
