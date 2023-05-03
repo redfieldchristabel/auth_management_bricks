eb4a751786272a2662dcc99923b535386ea58ab1
imano00 <nurimanjamal00@gmail.com>
2023-05-03 16:28:10 +0800

    add convert dart

--- base_user_model/__brick__/{{name.snakeCase()}}.dart
+++ base_user_model/__brick__/{{name.snakeCase()}}.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:auth_management/auth_management.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 {{#isSerializable}}import 'package:json_annotation/json_annotation.dart';
