7f5bb3d74d1ce8595374fcc935c56655265ffd5e
imano00 <nurimanjamal00@gmail.com>
2023-05-04 15:14:22 +0800

    initialize isar

--- auth_service/__brick__/auth_service.dart
+++ auth_service/__brick__/auth_service.dart
@@ -1,7 +1,9 @@
 import 'package:auth_management/services/base_auth_service.dart';
 
+
 class AuthService extends BaseAuthService {
     
+    get isar => BaseAuthService.isar;
 }
 
 final AuthService authService = AuthService();
