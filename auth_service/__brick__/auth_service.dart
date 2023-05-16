dd6450a39c8f23de775dec115ecfb202a2f11925
redfield <redfieldchristabel@gmail.com>
2023-05-16 13:34:11 +0800

    add new promt for user model name

--- auth_service/__brick__/auth_service.dart
+++ auth_service/__brick__/auth_service.dart
@@ -1,12 +1,12 @@
 import 'package:auth_management/auth_management.dart';
 import 'package:isar/isar.dart';
 
-class AuthService extends BaseAuthService {
+class AuthService extends BaseAuthService{{user_model.pascalCase()}} {
   get isar => BaseAuthService.isar;
 
   @override
   // TODO: implement usersIsar
-  IsarCollection<BaseUser> get usersIsar => throw UnimplementedError();
+  IsarCollection<{{user_model.pascalCase()}}> get usersIsar => throw UnimplementedError();
 }
 
 final AuthService authService = AuthService();
