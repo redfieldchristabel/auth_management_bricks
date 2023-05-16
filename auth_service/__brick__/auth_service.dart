241e7dfcb98e60c8f2c5f965b2b08e8183b3bfe1
redfield <redfieldchristabel@gmail.com>
2023-05-16 13:26:21 +0800

    redefine auth service base code

--- auth_service/__brick__/auth_service.dart
+++ auth_service/__brick__/auth_service.dart
@@ -1,9 +1,12 @@
-import 'package:auth_management/services/base_auth_service.dart';
-
+import 'package:auth_management/auth_management.dart';
+import 'package:isar/isar.dart';
 
 class AuthService extends BaseAuthService {
-    
-    get isar => BaseAuthService.isar;
+  get isar => BaseAuthService.isar;
+
+  @override
+  // TODO: implement usersIsar
+  IsarCollection<BaseUser> get usersIsar => throw UnimplementedError();
 }
 
 final AuthService authService = AuthService();
